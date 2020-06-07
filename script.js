482875eec002c5d2d0a9175ef4528fedfbbc1a31
Bart Kalanski <bkalanski@gmail.com>
2020-06-07 13:54:30 +0100

    Created basic structure

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.modal').modal();
+    $('.sidenav').sidenav();
+    $('.materialboxed').materialbox();
+    $('.parallax').parallax();
+    $('.tabs').tabs();
+    $('.datepicker').datepicker({
+      disableWeekends: true
+    });
+    $('.tooltipped').tooltip();
+    $('.scrollspy').scrollSpy();
+  });
